028014f34215363884103a2da6b5c8ff18e4b988
cnyanqun <cnyanqun@163.com>
2019-09-03 18:31:48 +0800

    blog

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-button type="text" @click="linkList">文字按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {},
+  methods: {
+    linkList() {
+      this.$router.push({
+        //核心语句
+        path: "/list", //跳转的路径
+      });
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <el-button type="text" disabled>list</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+  }
+}
+</script>
+<style scoped>
+</style>
