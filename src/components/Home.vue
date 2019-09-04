2bbd1409b4c4fd74722d787262f5ab94886507f8
cnyanqun <cnyanqun@163.com>
2019-09-04 17:59:33 +0800

    blogs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,41 @@
 <template>
-  <div class="aaa">
-    <el-button type="text" @click="linkList">文字按钮</el-button>
+  <div>
+    <el-row class="header">
+      <el-col :span="8">
+        <img src="@/assets/logo_1.png" alt="" class="logo">
+      </el-col>
+      <el-col :span="16" class="hidden-sm-and-down">右边</el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  data() {},
+  data() {
+    return {};
+  },
   methods: {
     linkList() {
       this.$router.push({
         //核心语句
-        path: "/list", //跳转的路径
+        path: "/list" //跳转的路径
       });
     }
   }
 };
 </script>
 <style lang="scss" scoped type="text/css">
-  .aaa{
-    background: $bg;
+.header {
+  background: $themeColor;
+  color: #fff;
+  height: 50px;
+  padding: 0 10px;
+  @include prefixer(box-shadow,0px 3px 5px #333333);
+  display: flex;
+  align-items: center;
+  .logo{
+    width: 50px;
   }
+}
 </style>
