90310788fe4eeac5f3e6d9d4dbe590c439c21eca
晏群 <cnyanqun@qq.com>
2020-01-06 16:12:04 +0800

    y

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,19 +1,18 @@
 <template>
   <div>
-    <Table1 />
+    <!-- <Table1 /> -->
     <Table2 />
   </div>
 </template>
 
 <script>
-import Table1 from "./table/Table1";
+// import Table1 from "./table/Table1";
 import Table2 from "./table/Table2";
 export default {
   data() {
     return {};
   },
   components: {
-    Table1,
     Table2
   }
 };
--- src/components/table/Table2.vue
+++ src/components/table/Table2.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-table :data="tableData" :span-method="arraySpanMethod" border style="width: 100%">
+  <el-table :data="tableData" :span-method="hang" border style="width: 100%">
     <el-table-column prop="id" label="ID" width="180"></el-table-column>
     <el-table-column prop="name" label="姓名"></el-table-column>
     <el-table-column prop="amount1" sortable label="数值 1"></el-table-column>
@@ -13,36 +13,36 @@ export default {
     return {
       tableData: [
         {
-          id: "12987122",
-          name: "王小虎",
+          id: "1",
+          name: "王小虎1",
           amount1: "234",
           amount2: "3.2",
           amount3: 10
         },
         {
-          id: "12987123",
-          name: "王小虎",
+          id: "2",
+          name: "王小虎2",
           amount1: "165",
           amount2: "4.43",
           amount3: 12
         },
         {
-          id: "12987124",
-          name: "王小虎",
+          id: "3",
+          name: "王小虎3",
           amount1: "324",
           amount2: "1.9",
           amount3: 9
         },
         {
-          id: "12987125",
-          name: "王小虎",
+          id: "4",
+          name: "王小虎4",
           amount1: "621",
           amount2: "2.2",
           amount3: 17
         },
         {
-          id: "12987126",
-          name: "王小虎",
+          id: "5",
+          name: "王小虎5",
           amount1: "539",
           amount2: "4.1",
           amount3: 15
@@ -52,11 +52,50 @@ export default {
   },
   methods: {
     arraySpanMethod({ row, column, rowIndex, columnIndex }) {
-			console.log(row, column, rowIndex, columnIndex)
-      if (rowIndex % 2 === 0) {
+      console.log(row, column, rowIndex, columnIndex);
+      //合并列
+      if (rowIndex === 0) {
         if (columnIndex === 0) {
-          return [1, 3];
-        } else if (columnIndex === 1||columnIndex === 2) {
+          //[1, 2]合并1行，2个单元格
+          return [1, 2];
+        } else if (columnIndex === 1) {
+          //上面合了哪一条我就得在这把这条整没
+          return [0, 0];
+        }
+      }
+    },
+    hang({ row, column, rowIndex, columnIndex }) {
+      console.log(row, column, rowIndex, columnIndex);
+      //合并行
+      if (columnIndex === 0 || columnIndex === 1 || columnIndex === 2) {
+        if (rowIndex === 0) {
+          //[1, 2]合并2行，1个单元格
+          return [3, 1];
+        } else if (rowIndex === 1 || rowIndex === 2) {
+          return [0, 0];
+        }
+      }
+    },
+    objectSpanMethod({ row, column, rowIndex, columnIndex }) {
+      console.log(row, column, rowIndex, columnIndex);
+      // 合并行
+      if (columnIndex === 0) {
+        if (rowIndex === 0) {
+          //[2,1]合并2行，1个单元格
+          return [3, 3];
+        } else if (rowIndex === 1) {
+          return [0, 0];
+        }else if (rowIndex === 2) {
+          return [0, 0];
+        }
+      } else {
+        if (rowIndex == 0 && (columnIndex === 1 || columnIndex === 2)) {
+          return [0, 0];
+        }
+        if (rowIndex === 1 && (columnIndex === 1 || columnIndex === 2)) {
+          return [0, 0];
+        }
+        if (rowIndex === 2 && (columnIndex === 1 || columnIndex === 2)) {
           return [0, 0];
         }
       }
